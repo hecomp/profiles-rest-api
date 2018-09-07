6ac12b97ca4fc3541d36960b091e32fa98b9778a
Heber Silva <hebercomp@yahoo.com>
2018-09-07 19:03:13 -0400

    Added a custom user profile model, and the migrations for our database.

--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Assinging a custom user model in django.
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
