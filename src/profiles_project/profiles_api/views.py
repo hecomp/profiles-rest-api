311c63f874ce420e54f3515fb7ad26390e524a2b
Heber Silva <hebercomp@yahoo.com>
2018-09-08 16:12:04 -0400

    Added POST, PUT, PATCH, and DELETE functions to our APIView

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
+
 
 # Create your views here.
 
 class HelloApiView(APIView):
     """Test API View."""
+
+    serializers_class = serializers.HelloSerializer
 
     def get(self, resquest, format=None):
         """Returns a list of APIview features."""
@@ -19,4 +25,18 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def post(self, resquest):
+        """Create a hello message with our name"""
+
+        serializer = serializers.HelloSerializer(data=resquest.data)
+
+        if(serializer.is_valid()):
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
         
